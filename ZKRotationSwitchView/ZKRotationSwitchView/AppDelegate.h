8c0060c2e449e60dba1c773585ddd4c79a909513
ZK <zk_520it@163.com>
2016-05-24 13:47:50 +0800

    sync

---
+++ ZKRotationSwitchView/ZKRotationSwitchView/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  ZKRotationSwitchView
+//
+//  Created by ZK on 16/5/24.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ ZKRotationSwitchView/ZKRotationSwitchView/Classes/ZKMainViewController.m
@@ -0,0 +1,22 @@
+//
+//  ZKMainViewController.m
+//  ZKRotationSwitchView
+//
+//  Created by ZK on 16/5/24.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+#import "ZKMainViewController.h"
+
+@interface ZKMainViewController ()
+
+@end
+
+@implementation ZKMainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
